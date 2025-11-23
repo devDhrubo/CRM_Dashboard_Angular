92cbcdf5028a1374119c1f4246bc6ab0b7d459f5
devDhrubo <dolandhruboray@gmail.com>
2025-11-23 18:20:36 +0600

    Right Section Completed

--- src/app/components/main-body/main-body.ts
+++ src/app/components/main-body/main-body.ts
@@ -3,11 +3,12 @@ import { Button } from 'primeng/button';
 import { TabsModule } from 'primeng/tabs';
 import { AvatarModule } from 'primeng/avatar';
 import { AvatarGroupModule } from 'primeng/avatargroup';
+import { RightSidebar } from "../right-sidebar/right-sidebar";
 
 
 @Component({
   selector: 'app-main-body',
-  imports: [TabsModule, Button, AvatarModule, AvatarGroupModule],
+  imports: [TabsModule, Button, AvatarModule, AvatarGroupModule, RightSidebar],
   templateUrl: './main-body.html',
   styleUrl: './main-body.css',
 })
---
+++ src/app/components/right-sidebar/right-sidebar.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import { AvatarModule, Avatar } from 'primeng/avatar';
+import { AvatarGroupModule } from 'primeng/avatargroup';
+
+
+@Component({
+  selector: 'app-right-sidebar',
+  imports: [Avatar],
+  templateUrl: './right-sidebar.html',
+  styleUrl: './right-sidebar.css',
+})
+export class RightSidebar {
+
+}
